b665360c270005e2b8b6654dbf8af0e5fadc8ab3
saluton1 <appereater@gmail.com>
2020-03-21 16:12:36 +0000

    regular expression matching

--- python_code/regular_expression_matching.py
+++ python_code/regular_expression_matching.py
@@ -1,51 +1,24 @@
 class Solution:
-    def isMatch(self, s: str, p: str) -> bool:
-        if not s and not p:
-            return True
-        elif not s or not p:
-            return False
-        flag = 0
-        n_recog_s = 0
-        number_j = 0
-        number_p = 0
-        for i in s:
-            for j in p[flag:]:
-                if i == j:
-                    flag += 1
-                    number_p += 1
-                    n_recog_s += 1
-                    number_j += 1
-                    break
-                elif j == '.':
-                    flag += 1
-                    n_recog_s += 1
-                    number_j += 1
-                    number_p += 1
-                    break
-                elif j == '*':
-                    
-                    if p[number_j - 1] == i:
-                        if flag + 1 == len(s):
-                            number_p += 1
-                        n_recog_s += 1
-                        break
-                    elif p[number_j - 1] == '.':
-                        if flag + 1 == len(s):
-                            number_p += 1
-                        n_recog_s += 1
-                        break
+    def isMatch(self, s, p):
+
+        dp = [[True] + [False] * len(s)]
+        for i in range(len(p)):
+            dp.append([False] * (len(s) + 1))
+        for i in range(1, len(p) + 1):
+            if p[i - 1] == '*':
+                dp[i][0] = dp[i - 2][0]
+        for i in range(1, len(p) + 1):
+            for j in range(1, len(s) + 1):
+                if p[i - 1] in {s[j - 1], '.'}:
+                    dp[i][j] = dp[i - 1][j - 1]
+                elif p[i - 1] == '*':
+                    if p[i - 2] == s[j - 1] or p[i - 2] == '.':
+                        dp[i][j] = dp[i][j - 1] or dp[i - 2][j]
                     else:
-                        flag += 1
-                        number_j += 1
-                        number_p += 1
-                else:
-                    flag += 1
-                    number_j += 1
-                    number_p += 1
+                        dp[i][j] = dp[i-2][j]
+        return dp[-1][-1]
 
-        return True if n_recog_s == len(s) and number_p == len(p) else False
 
-res = Solution()
-re = res.isMatch("aa",
-            "a*")
-print(re)
+solution = Solution()
+res = solution.isMatch('','.*')
+print(res)
