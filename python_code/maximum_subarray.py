a5f9d585f6cb4d473c1a67c9d8f69c560b7b180d
saluton1 <appereater@gmail.com>
2020-03-21 18:28:39 +0000

    maximun subarray

--- python_code/maximum_subarray.py
+++ python_code/maximum_subarray.py
@@ -1,16 +1,10 @@
 class Solution:
     def maxSubArray(self, nums):
-        le = len(nums)
-        array = [[0] * (le + 1) for _ in range(le + 1)]
-        res = nums[0]
-        for i in range(1, le + 1):
-            for j in range(i, le + 1):
-                array[i][j] = array[i][j-1] + nums[j - 1]
-                print(array[i][j - 1], nums[j - 1])
-            res = max(max(array[i][1:]), res)
-        print(array)
-        print(res)
+        res = cur_sum = nums[0]
+        for i in nums[1:]:
+            cur_sum = max(i, cur_sum + i)
+            res = max(res, cur_sum)
         return res
 
-res = Solution()
-res.maxSubArray([-2, -1])
+
+
